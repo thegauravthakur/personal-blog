f1bc2a1f4b35225dbb166a07a5612e8d62eddc62
Gaurav Thakur <gthakur581@gmail.com>
2021-12-05 21:51:39 +0530

    make nav responsive

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,5 +1,5 @@
 import { css } from '@emotion/react';
-
+import { AiFillAccountBook, AiOutlineMenu } from 'react-icons/ai';
 export function Nav() {
   return (
     <nav
@@ -10,22 +10,42 @@ export function Nav() {
         justify-content: space-between;
         align-items: center;
         padding: 0 60px;
+        @media (max-width: 930px) {
+          padding: 0 50px;
+        }
+        @media (max-width: 780px) {
+          padding: 0 40px;
+        }
+        @media (max-width: 700px) {
+          padding: 0 20px;
+        }
       `}
     >
-      <h1 css={css``}>Gaurav's Blog</h1>
+      <h1
+        css={(theme) => css`
+          font-size: 20px;
+          color: ${theme.color.text.main};
+        `}
+      >
+        Gaurav's Blog
+      </h1>
       <ul
-        css={css`
+        css={(theme) => css`
           list-style: none;
+          color: ${theme.color.text.main};
           display: flex;
+          @media (max-width: 740px) {
+            display: none;
+          }
           & > li {
             min-width: 100px;
             display: inline-block;
-            padding: 20px 0 15px 0;
+            padding: 16px 0 11px 0;
             cursor: pointer;
             text-align: center;
             border-bottom: 5px solid transparent;
             &:hover {
-              border-bottom: 5px solid red;
+              border-bottom: 5px solid ${theme.color.link.main};
             }
           }
         `}
@@ -34,6 +54,15 @@ export function Nav() {
         <li>Portfolio</li>
         <li>Theme</li>
       </ul>
+      <AiOutlineMenu
+        css={css`
+          display: none;
+          @media (max-width: 740px) {
+            display: inline-block;
+            margin: 20px 0;
+          }
+        `}
+      />
     </nav>
   );
 }
