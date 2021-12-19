693ede203bc6a5d2f4dd9285bea906cb6e856d83
Gaurav Thakur <gthakur581@gmail.com>
2021-12-19 12:01:30 +0530

    update the types file

--- src/page-components/index/utils.ts
+++ src/page-components/index/utils.ts
@@ -1,9 +1,9 @@
-import { Post } from './Index.types';
+import { Article } from './Index.types';
 
-export const comparator = (post1: Post, post2: Post) => {
-  const date1 = new Date(post1.metaData.publishedDate);
-  const date2 = new Date(post2.metaData.publishedDate);
-  if (date1 < date2) return 1;
-  if (date1 > date2) return -1;
+export const comparator = (articleA: Article, articleB: Article) => {
+  const dateA = new Date(articleA.metaData.publishedDate);
+  const dateB = new Date(articleB.metaData.publishedDate);
+  if (dateA < dateB) return 1;
+  if (dateA > dateB) return -1;
   return 0;
 };
