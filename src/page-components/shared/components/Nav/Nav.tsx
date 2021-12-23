db0501e47f2ad6f6338a17e6b0bb5ae784ebad44
Gaurav Thakur <gthakur581@gmail.com>
2021-12-23 23:00:31 +0530

    add twin in nav

--- src/page-components/shared/components/Nav/Nav.tsx
+++ src/page-components/shared/components/Nav/Nav.tsx
@@ -1,69 +1,42 @@
 import { css } from '@emotion/react';
 import { AiOutlineMenu } from 'react-icons/ai';
-
 import { useContext, useEffect, useState } from 'react';
-
 import useLayoutEffect from '../../../../hooks/useIsomorphicLayoutEffect';
-
 import Link from 'next/link';
-
 import { BsFillMoonFill, BsSunFill } from 'react-icons/bs';
-
 import { sun, moon } from '../../../../styles/animation';
-
 import { RiMoonFill } from 'react-icons/ri';
-
 import { ThemeContext } from '../../../../styles/theme';
-
 import Logo from '../Logo';
-
 import ThemeTransition from './component/ThemeTransition';
-
-import {
-  StyledHeader,
-  StyledNav,
-  StyledToolbox,
-  ToolboxItem,
-  ToolBoxItemLink,
-} from './component/StyledComponents';
-
-import {
-  burgerMenuStyles,
-  mobileMoonStyles,
-  mobileSunStyles,
-  ToolboxWrapper,
-} from './Nav.styles';
-
 import MobileMenu from './component/MobileMenu';
-
 import { Constant } from '../../utils';
-
 import tw from 'twin.macro';
+
+export const focusStyles = css({
+  '&:focus-within': tw`outline-width[3px] outline-style[dotted] outline-offset[3px] outline-color[var(--primary-main)]`,
+});
+const listStyle = tw`min-w-[100px] flex justify-center items-center h-full cursor-pointer text-center border-solid border-0 border-t-[5px] border-b-[5px] border-transparent hover:border-b-red-600 hover:dark:border-b-blue-600 transition-colors ease-in duration-100`;
+const menuItemStyle = tw`no-underline text-current outline-none h-full flex items-center w-full justify-center`;
+const mobileIconStyles = tw`md:hidden dark:text-gray-400 inline-block text-current self-center p-2.5 rounded-full cursor-pointer transition-colors ease-in duration-300 dark:hocus:bg-gray-800 hocus:(bg-rose-100)`;
 
 export function Nav() {
   const { theme, setTheme } = useContext(ThemeContext);
-
   const [showMobileNav, setShowMobileNav] = useState(false);
-
   const [animate, setAnimate] = useState(false);
 
   const onThemeChange = () => {
     const updatedTheme = theme === 'dark' ? 'light' : 'dark';
-
     document.body.dataset.theme = updatedTheme;
-
     if (updatedTheme === 'light')
       document.documentElement.classList.remove('dark');
     else document.documentElement.classList.add('dark');
-
     localStorage.setItem('theme', updatedTheme);
-
     setTheme(updatedTheme);
   };
 
   useLayoutEffect(() => {
     const { theme } = document.body.dataset;
-
     if (theme) setTheme(theme as 'light' | 'dark');
   }, [setTheme]);
 
@@ -71,21 +44,18 @@ export function Nav() {
     setAnimate(true);
   }, []);
 
-  const focusStyles = css({
-    '&:focus-within': tw`outline-width[3px] outline-style[dotted] outline-offset[3px] outline-color[var(--primary-main)]`,
-  });
-
   return (
     <header>
       {animate && <ThemeTransition />}
 
-      <StyledNav>
+      <nav
+        css={tw`bg-gray-50 dark:bg-background-800 flex justify-between items-center py-0 px-3 sm:px-6 md:px-10 `}
+      >
         <h1>
           <Link href='/' passHref>
             <a
               css={[
                 tw`flex items-center gap-x-2 no-underline text-xl text-rose-700 dark:text-gray-400 transition-colors ease-in duration-100`,
-
                 focusStyles,
               ]}
             >
@@ -95,30 +65,30 @@ export function Nav() {
           </Link>
         </h1>
 
-        <div css={ToolboxWrapper}>
-          <StyledToolbox>
-            <ToolboxItem>
+        <div css={tw`h-[60px] flex justify-center self-center`}>
+          <ul
+            css={tw`list-none h-full items-center text-slate-800 dark:text-gray-400 hidden md:flex`}
+          >
+            <li css={listStyle}>
               <Link href={'/'} passHref>
-                <ToolBoxItemLink>Home</ToolBoxItemLink>
+                <a css={menuItemStyle}>Home</a>
               </Link>
-            </ToolboxItem>
+            </li>
 
-            <ToolboxItem>
+            <li css={listStyle}>
               <Link href={Constant.portfolio} passHref>
-                <ToolBoxItemLink>Portfolio</ToolBoxItemLink>
+                <a css={menuItemStyle}>Portfolio</a>
               </Link>
-            </ToolboxItem>
+            </li>
 
-            <ToolboxItem
+            <li
+              css={[listStyle, tw`gap-x-2.5`]}
               onKeyDown={(e) => {
                 if (e.key === 'Enter') {
                   onThemeChange();
                 }
               }}
               tabIndex={0}
-              css={css`
-                column-gap: 10px;
-              `}
               onClick={onThemeChange}
             >
               Theme
@@ -136,36 +106,39 @@ export function Nav() {
                   size={20}
                 />
               )}
-            </ToolboxItem>
-          </StyledToolbox>
+            </li>
+          </ul>
 
           {!showMobileNav && (
             <AiOutlineMenu
+              tabIndex={0}
               onClick={() => {
                 setShowMobileNav(!showMobileNav);
 
                 document.body.style.overflow = 'hidden';
               }}
               size={38}
-              css={burgerMenuStyles}
+              css={mobileIconStyles}
             />
           )}
 
           {theme === 'dark' ? (
             <RiMoonFill
+              tabIndex={0}
               onClick={onThemeChange}
               size={40}
-              css={mobileMoonStyles}
+              css={[mobileIconStyles]}
             />
           ) : (
             <BsSunFill
+              tabIndex={0}
               size={40}
               onClick={onThemeChange}
-              css={mobileSunStyles}
+              css={mobileIconStyles}
             />
           )}
         </div>
-      </StyledNav>
+      </nav>
 
       {showMobileNav && <MobileMenu setShowMobileNav={setShowMobileNav} />}
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,23 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      keyframes: {
+        moon: {
+          '0%': { transform: 'rotate(50deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },
+      animation: {
+        moon: 'moon 0.3s ease-out',
+      },
       colors: {
         rose: colors.rose,
+        slate: {
+          800: '#1E293B',
+        },
+        background: {
+          800: '#161b22',
+        },
       },
     },
   },
