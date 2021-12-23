f6891629d9e3b280304df0df0a8d07204649d2d6
Gaurav Thakur <gthakur581@gmail.com>
2021-12-23 14:18:20 +0530

    add twin and tailwind support

--- src/page-components/shared/components/Nav/Nav.tsx
+++ src/page-components/shared/components/Nav/Nav.tsx
@@ -1,14 +1,24 @@
 import { css } from '@emotion/react';
 import { AiOutlineMenu } from 'react-icons/ai';
+
 import { useContext, useEffect, useState } from 'react';
+
 import useLayoutEffect from '../../../../hooks/useIsomorphicLayoutEffect';
+
 import Link from 'next/link';
+
 import { BsFillMoonFill, BsSunFill } from 'react-icons/bs';
+
 import { sun, moon } from '../../../../styles/animation';
+
 import { RiMoonFill } from 'react-icons/ri';
+
 import { ThemeContext } from '../../../../styles/theme';
+
 import Logo from '../Logo';
+
 import ThemeTransition from './component/ThemeTransition';
+
 import {
   StyledHeader,
   StyledNav,
@@ -16,38 +26,51 @@ import {
   ToolboxItem,
   ToolBoxItemLink,
 } from './component/StyledComponents';
+
 import {
   burgerMenuStyles,
   mobileMoonStyles,
   mobileSunStyles,
   ToolboxWrapper,
 } from './Nav.styles';
+
 import MobileMenu from './component/MobileMenu';
+
 import { Constant } from '../../utils';
+
 import tw from 'twin.macro';
 
 export function Nav() {
   const { theme, setTheme } = useContext(ThemeContext);
+
   const [showMobileNav, setShowMobileNav] = useState(false);
+
   const [animate, setAnimate] = useState(false);
 
   const onThemeChange = () => {
     const updatedTheme = theme === 'dark' ? 'light' : 'dark';
+
     document.body.dataset.theme = updatedTheme;
+
     if (updatedTheme === 'light')
       document.documentElement.classList.remove('dark');
     else document.documentElement.classList.add('dark');
+
     localStorage.setItem('theme', updatedTheme);
+
     setTheme(updatedTheme);
   };
+
   useLayoutEffect(() => {
     const { theme } = document.body.dataset;
+
     if (theme) setTheme(theme as 'light' | 'dark');
   }, [setTheme]);
 
   useEffect(() => {
     setAnimate(true);
   }, []);
+
   const focusStyles = css({
     '&:focus-within': tw`outline-width[3px] outline-style[dotted] outline-offset[3px] outline-color[var(--primary-main)]`,
   });
@@ -55,12 +78,14 @@ export function Nav() {
   return (
     <header>
       {animate && <ThemeTransition />}
+
       <StyledNav>
         <h1>
           <Link href='/' passHref>
             <a
               css={[
-                tw`flex items-center gap-x-2 no-underline text-xl text-rose-700 dark:text-gray-300 transition-colors ease-in duration-100`,
+                tw`flex items-center gap-x-2 no-underline text-xl text-rose-700 dark:text-gray-400 transition-colors ease-in duration-100`,
+
                 focusStyles,
               ]}
             >
@@ -69,6 +94,7 @@ export function Nav() {
             </a>
           </Link>
         </h1>
+
         <div css={ToolboxWrapper}>
           <StyledToolbox>
             <ToolboxItem>
@@ -76,11 +102,13 @@ export function Nav() {
                 <ToolBoxItemLink>Home</ToolBoxItemLink>
               </Link>
             </ToolboxItem>
+
             <ToolboxItem>
               <Link href={Constant.portfolio} passHref>
                 <ToolBoxItemLink>Portfolio</ToolBoxItemLink>
               </Link>
             </ToolboxItem>
+
             <ToolboxItem
               onKeyDown={(e) => {
                 if (e.key === 'Enter') {
@@ -110,16 +138,19 @@ export function Nav() {
               )}
             </ToolboxItem>
           </StyledToolbox>
+
           {!showMobileNav && (
             <AiOutlineMenu
               onClick={() => {
                 setShowMobileNav(!showMobileNav);
+
                 document.body.style.overflow = 'hidden';
               }}
               size={38}
               css={burgerMenuStyles}
             />
           )}
+
           {theme === 'dark' ? (
             <RiMoonFill
               onClick={onThemeChange}
@@ -135,6 +166,7 @@ export function Nav() {
           )}
         </div>
       </StyledNav>
+
       {showMobileNav && <MobileMenu setShowMobileNav={setShowMobileNav} />}
     </header>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require('tailwindcss/colors');
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        rose: colors.rose,
+      },
+    },
+  },
+  plugins: [],
+};
