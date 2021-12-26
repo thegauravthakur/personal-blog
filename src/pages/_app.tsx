3a9ee82487331c59e4a70857fb3e436e5bf53549
Gaurav Thakur <gthakur581@gmail.com>
2021-12-26 20:41:58 +0530

    Add Google Analytics support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 // import '../styles/globals.css';
 import 'modern-normalize/modern-normalize.css';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import type { AppProps } from 'next/app';
 import { ThemeContext, Theme } from '../styles/theme';
 import { GlobalStyles } from '../styles/GlobalStyles';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Nav } from '../page-components/shared/components/Nav';
 import CustomHead from '../page-components/app/components/CustomHead';
+import * as gtag from '../lib/gtag';
+import Script from 'next/script';
 
 NProgress.configure({ showSpinner: false });
 
@@ -18,13 +20,45 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = useState<Theme>('dark');
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <CustomHead theme={theme} />
-      <GlobalStyles />
-      <Nav />
-      <Component {...pageProps} />
-    </ThemeContext.Provider>
+    <>
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id='gtag-init'
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <ThemeContext.Provider value={{ theme, setTheme }}>
+        <CustomHead theme={theme} />
+        <GlobalStyles />
+        <Nav />
+        <Component {...pageProps} />
+      </ThemeContext.Provider>
+    </>
   );
 }
 
