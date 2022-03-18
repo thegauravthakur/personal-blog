a2a0a74a8599ce8397c38dbc30d6d26ee1219e36
Gaurav Thakur <gthakur581@gmail.com>
2022-03-18 15:37:18 +0530

    Fix typescript issues

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,17 +1,20 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: string) => {
-    // @ts-ignore
-    window.gtag('config', GA_TRACKING_ID, {
+    (window as any).gtag('config', GA_TRACKING_ID, {
         page_path: url,
     });
 };
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }: any) => {
-    // @ts-ignore
-    window.gtag('event', action, {
+interface EventProps {
+    action: string;
+    category: string;
+    label: string;
+    value: string;
+}
+
+export const event = ({ action, category, label, value }: EventProps) => {
+    (window as any).gtag('event', action, {
         event_category: category,
         event_label: label,
         value,
